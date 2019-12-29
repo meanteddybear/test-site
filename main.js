19d810a1dd399fc002e20ca6ee4ed57dd5ec5eae
Scott Carter <scott.carter92@gmail.com>
2019-12-29 13:05:54 -0600

    updating

--- main.js
+++ main.js
@@ -1,27 +1,65 @@
-const myForm = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
-const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
+//make a function
 
-myForm.addEventListener('submit', onSubmit);
+function game() {
 
-function onSubmit(e) {
-    e.preventDefault();
+    let totalRounds = 0;
+    let playerWins = 0;
+    let computerWins = 0;
+    let tiedRounds = 0;
 
-    if(nameInput.value === '' || emailInput.value === '') {
-        msg.classList.add('error');
-        msg.innerHTML = 'Please enter all fields';
+    while (totalRounds <= 5) {
+        const computerSelection = computerPlay()
+        const playerSelection = window.prompt('Rock, paper, scissors?');
 
-        setTimeout(() => msg.remove(), 3000);
-    } else {
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        function computerPlay() {
+            let randomNum = Math.floor(Math.random() * Math.floor(3))
+            if (randomNum === 0) {
+                return "rock"
+            } else if (randomNum === 1) {
+                return "paper"
+            } else if (randomNum === 2)
+                return "scissors"
+        }
 
-        userList.appendChild(li);
+        //make a loop that will run playRound 5 times, keep and return the score.
+        function playRound(playerSelection, computerSelection) {
 
-        //Clear fields
-        nameInput.value = '';
-        emailInput.value = '';
+            console.log('player picked ' + playerSelection)
+            console.log('computer picked ' + computerSelection)
+
+            if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'scissors') {
+                playerWins++;
+                return ('You win, rock beats scissors.');
+            } else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'paper') {
+                computerWins++;
+                return ('You lose, Paper beats rock.');
+            } else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'rock') {
+                tiedRounds++;
+                return ('You tied.');
+            } else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'rock') {
+                playerWins++;
+                return ('You win, paper beats rock.');
+            } else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'scissors') {
+                computerWins++;
+                return ('You lose, Scissors beats paper.');
+            } else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'paper') {
+                tiedRounds++;
+                return ('You tied.');
+            } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'paper') {
+                playerWins++;
+                return ('You win, scissors beats paper.');
+            } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'rock') {
+                computerWins++;
+                return ('You lose, rock beats scissors.');
+            } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'scissors') {
+                tiedRounds++;
+                return ('You tied.');
+            }
+        }
+        console.log('--------------------------')
+        console.log(playRound(playerSelection, computerSelection))
+        totalRounds++;
+        console.log('Player: ' + playerWins + '.' + ' // ' + 'Computer: ' + computerWins + '.' +' // '+ 'Tied: ' + tiedRounds + '.'  )
     }
 }
+game()
